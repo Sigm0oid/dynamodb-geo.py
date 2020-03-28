eaa9148c0fe9df10954992d25035e6da61596b4e
Walid <walidsad977@gmail.com>
2020-03-28 19:35:18 +0100

    added query rectangle request model

--- src/s2/S2Manager.py
+++ src/s2/S2Manager.py
@@ -1,13 +1,15 @@
 from s2sphere import LatLng as S2LatLng
 from s2sphere import Cell as S2Cell
-from model import GeoPoint
+
 
 class S2Manager:
     def generateGeohash(self, geoPoint):
-        latLng = S2LatLng.from_degrees(geoPoint.getLatitude(), geoPoint.getLongitude())
+        latLng = S2LatLng.from_degrees(
+            geoPoint.getLatitude(), geoPoint.getLongitude())
         cell = S2Cell.from_lat_lng(latLng)
         cellId = cell.id()
         return cellId.id()
+
     def generateHashKey(self, geohash, hashKeyLength):
         if geohash < 0:
             hashKeyLength += 1
