a90980933328b07cfec3e7ec27f8cc884dd0803b
Walid <walidsad977@gmail.com>
2020-03-29 11:56:48 +0100

    added get point to dynamoDBManager

--- src/DynamoDBManager.py
+++ src/DynamoDBManager.py
@@ -4,29 +4,53 @@ Purpose: This class contains all the operation to perform on the DynamoDB table
 """
 from s2.S2Manager import S2Manager
 from model.PutPointInput import PutPointInput
+
+
 class DynamoDBManager:
-        
-    def __init__(self,config):
-        self.config=config
-    
-    def put_Point(self,putPointInput):
+
+    def __init__(self, config):
+        self.config = config
+
+    def put_Point(self, putPointInput):
         """
         The dict in Item put_item call, should contains a dict with string as a key and a string as a value: {"N": "123"}
         """
         geohash = S2Manager().generateGeohash(putPointInput.GeoPoint)
         hashKey = S2Manager().generateHashKey(geohash, self.config.hashKeyLength)
-        response=""
+        response = ""
         try:
-            response=self.config.dynamoDBClient.put_item(
-                TableName=self.config.tableName, 
+            response = self.config.dynamoDBClient.put_item(
+                TableName=self.config.tableName,
                 Item={
-                    self.config.hashKeyAttributeName:{"N":str(hashKey)},
-                    self.config.rangeKeyAttributeName:{"S":putPointInput.RangeKeyValue},
-                    self.config.geohashAttributeName:{'N':str(geohash)},
-                    self.config.geoJsonAttributeName:{"S":str(putPointInput.ExtraFields)}
+                    self.config.hashKeyAttributeName: {"N": str(hashKey)},
+                    self.config.rangeKeyAttributeName: {"S": putPointInput.RangeKeyValue},
+                    self.config.geohashAttributeName: {'N': str(geohash)},
+                    self.config.geoJsonAttributeName: {
+                        "S": str(putPointInput.ExtraFields)}
                 }
             )
         except Exception as e:
             print("The following error occured during the item insertion :{}".format(e))
-            response= "Error"
+            response = "Error"
+        return response
+
+    def get_Point(self, getPointInput):
+        """
+        The dict in Key get_item call, should contains a dict with string as a key and a string as a value: {"N": "123"}
+        """
+        geohash = S2Manager().generateGeohash(getPointInput.GeoPoint)
+        hashKey = S2Manager().generateHashKey(geohash, self.config.hashKeyLength)
+        response = ""
+        try:
+            response = self.config.dynamoDBClient.get_item(
+                TableName=self.config.tableName,
+                key={
+                    self.config.hashKeyAttributeName: {"N": str(hashKey)},
+                    self.config.rangeKeyAttributeName: {
+                        "S": getPointInput.RangeKeyValue}
+                }
+            )
+        except Exception as e:
+            print("The following error occured during the item retrieval :{}".format(e))
+            response = "Error"
         return response
