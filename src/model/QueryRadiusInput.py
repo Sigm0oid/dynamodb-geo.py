b547d0b2d99527f752d763face986fde9ba8065e
Walid <walidsad977@gmail.com>
2020-03-29 17:31:41 +0100

    added QueryRadiusInput model

---
+++ src/model/QueryRadiusInput.py
@@ -0,0 +1,10 @@
+class QueryRadiusInput:
+    def __init__(self, centerPoint, radiusInMeter):
+        self.centerPoint = centerPoint
+        self.radiusInMeter = radiusInMeter
+
+    def getCenterPoint(self):
+        return self.centerPoint
+
+    def getRadiusInMeter(self):
+        return self.radiusInMeter
