b89453db0be3437b645d5d3ad2a5b26ffabb90b6
Walid <walidsad977@gmail.com>
2020-03-29 11:49:26 +0100

    added GetPointInput model

--- src/model/GetPointInput.py
+++ src/model/GetPointInput.py
@@ -1,4 +1,4 @@
 class GetPointInput:
-    def __init__(self, GeoPoint, RangeKeyValue, ExtraFields):
+    def __init__(self, GeoPoint, RangeKeyValue):
         self.GeoPoint = GeoPoint
         self.RangeKeyValue = RangeKeyValue
