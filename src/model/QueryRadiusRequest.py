027cfea9c44c74f1b8edf1166a82f43c41a98f1d
Walid <walidsad977@gmail.com>
2020-04-03 12:19:03 +0100

    added query radius to geoDataManager

--- src/model/QueryRadiusRequest.py
+++ src/model/QueryRadiusRequest.py
@@ -1,15 +1,16 @@
-from .GeoQueryInput import GeoQueryInput 
+from .GeoQueryInput import GeoQueryInput
+
 
 class QueryRadiusRequest(GeoQueryInput):
 
-    def __init__(self, centerPoint, radiusInMeter,query_input_dict={}):
+    def __init__(self, centerPoint, radiusInMeter, query_input_dict={}):
+        GeoQueryInput.__init__(self, query_input_dict)
         self.centerPoint = centerPoint
         self.radiusInMeter = radiusInMeter
-        super().__init__(self,query_input_dict)
+        # super().__init__(self,query_input_dict)
 
     def getCenterPoint(self):
         return self.centerPoint
 
     def getRadiusInMeter(self):
         return self.radiusInMeter
-
