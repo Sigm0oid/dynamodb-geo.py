71ab9d9c8abfc7f1dc857be90a8550643abde004
Walid <walidsad977@gmail.com>
2020-04-08 21:58:46 +0100

    fixed annotations

--- src/model/QueryRadiusRequest.py
+++ src/model/QueryRadiusRequest.py
@@ -1,15 +1,14 @@
 from GeoQueryInput import GeoQueryInput
-from GeoPoint import GeoPoint
 
 
 class QueryRadiusRequest(GeoQueryInput):
 
-    def __init__(self, centerPoint: GeoPoint, radiusInMeter: int, query_input_dict: dict = {}):
+    def __init__(self, centerPoint: 'GeoPoint', radiusInMeter: int, query_input_dict: dict = {}):
         GeoQueryInput.__init__(self, query_input_dict)
         self.centerPoint = centerPoint
         self.radiusInMeter = radiusInMeter
 
-    def getCenterPoint(self) -> GeoPoint:
+    def getCenterPoint(self) -> 'GeoPoint':
         return self.centerPoint
 
     def getRadiusInMeter(self) -> int:
